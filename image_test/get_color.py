6071acd4dbad030629332cd9fc9a1d44fce95f90
eumatheusfig <eumatheusfigueiredo@gmail.com>
2024-02-21 20:29:10 -0400

    Código de imagem
    Código que captura imagens funcionando

--- image_test/get_color.py
+++ image_test/get_color.py
@@ -1,7 +1,19 @@
 from PIL import Image
+import os
+
+
+
+
+# código para buscar os arquivos no diretório
+
+# print("Diretório de trabalho atual:", os.getcwd())                   # Verifica o diretório de trabalho atual
+# print("Arquivos na pasta:", os.listdir())                            # Lista os arquivos na pasta atual
+
+
+# código de detecção de cores
 
 img = Image.open("image_kratos.png")
 
-max_colors = 1000      # parâmetro definido para setar um limite de cores
+max_colors = 900000      # parâmetro definido para setar um limite de cores
 
 print(img.getcolors(max_colors))
